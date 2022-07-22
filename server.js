70d84789f823b4b76783bd44942f1dee1d1145c8
Andrew <atklenner@gmail.com>
2022-07-21 22:02:01 -0700

    finish 3.16

--- server.js
+++ server.js
@@ -12,31 +12,47 @@ app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :post")
 );
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  console.log(error.name);
+  if ((error.name = "CastError")) {
+    return res.status(400).send({ error: error.message });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
 morgan.token("post", (req, res) => {
   if (req.method === "POST") {
     return JSON.stringify(req.body);
   } else return "";
 });
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((people) => {
-    res.json(people);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((people) => {
+      res.json(people);
+    })
+    .catch((err) => next(err));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const { name, number } = req.body;
   if (name && number) {
     const person = new Person({ name, number });
-    person.save().then((newPerson) => {
-      res.json(newPerson);
-    });
-  } else {
-    res.status(400).json({ error: "name or number missing" });
+    person
+      .save()
+      .then((newPerson) => {
+        res.json(newPerson);
+      })
+      .catch((err) => next(err));
   }
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
@@ -46,19 +62,17 @@ app.get("/api/persons/:id", (req, res) => {
       }
     })
     .catch((err) => {
-      console.log(err);
-      res.status(500).end();
+      next(err);
     });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((result) => {
       res.status(204).end();
     })
     .catch((err) => {
-      console.log(err);
-      res.status(500).end();
+      next(err);
     });
 });
 
@@ -68,5 +82,7 @@ app.get("/api/info", (req, res) => {
     res.json(info);
   });
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log("Running"));
